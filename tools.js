23e24f598a06c98393335187196d9095b4d5c5ef
zxy <304246725@qq.com>
2015-09-21 05:12:45 +0800

    update extend,add dataSet

--- tools.js
+++ tools.js
@@ -41,11 +41,17 @@ var EleAttr=function(id,Attname,value){
 		else if(attrName==Attname)return ele.attributes[i].nodeValue;
 	}
 };
-//扩展
-function extend(obj, extension){
-    for(var key in extension){
-        if(extension.hasOwnProperty(key) && obj[key] == null){
-            obj[key] = extension[key];
+//传入两个以上参数，依次扩展第一个obj
+function extend(obj,extensions){
+    if(arguments.length < 2){
+        return obj;
+    }
+    var obj_ar = Array.prototype.slice.call(arguments,1);
+    for(var i = 0,len = obj_ar.length;i < len;i++){
+        for(var key in obj_ar[i]){
+            if(!obj.hasOwnProperty(key)&&obj_ar[i].hasOwnProperty(key)){
+                obj[key] = obj_ar[i][key];
+            }
         }
     }
     return obj;
@@ -56,4 +62,33 @@ function cancelHandler(event){
     if(event.preventDefault) event.preventDefault();
     if(event.cancelBubble) event.cancelBubble = false;
     return false;
+}
+
+//dataset兼容
+function dataSet(ele,data_name,data){
+    function name(str){
+        var name_ar = [];
+        var reg = /[A-Z]/g;
+        var result = reg.exec(str);
+        for(var i = 0,j = 0,k = 0;result;i++,result = reg.exec(str)){
+            k = result.index;
+            name_ar[i] = str.substring(j,k).toLowerCase();
+            j = k;
+        }
+        name_ar[i] = str.substring(j).toLowerCase();
+        return 'data-'+name_ar.join("-");
+    }
+    if(ele.dataset){
+        if(data){
+            ele.dataset[data_name] = data;
+        }
+        return ele.dataset[data_name];
+    }
+    else{
+        var dataName = name(data_name);
+        if(data){
+            ele.setAttribute(dataName,data);
+        }
+        return ele.getAttribute(dataName);
+    }
 }
